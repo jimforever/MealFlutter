6a41b3208ee1c397477a849606c7c09f2e63f66d
吴建飞 <jim.wu@alpha-ess.com>
2022-11-04 13:41:26 +0800

    lesson 174 Adding a TabBar to the Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meal_flutter/screens/categories_screen.dart';
 import 'package:meal_flutter/screens/category_meals_screen.dart';
 import 'package:meal_flutter/screens/meal_detail_screen.dart';
+import 'package:meal_flutter/screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,58 +15,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
           primarySwatch: Colors.pink,
           accentColor: Colors.amber,
-          canvasColor: const Color.fromRGBO(255,254,229,1),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
           textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText1: const TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            bodyText2: const TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            headline6: const TextStyle(
-              fontSize: 20,
-            ),
-          )
-      ),
-      home: const CategoriesScreen(),
+                bodyText1: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodyText2: const TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                headline6: const TextStyle(
+                  fontSize: 20,
+                ),
+              )),
+      initialRoute: '/',
       routes: {
-        '/category-meals':(ctx) => const CategoryMealsScreen(),
-        // '/meal-detail':(ctx) => const MealDetailScreen()
-    },
-      onGenerateRoute: (settings){
-        print("onGenerateRoute");
-        print(settings.arguments);
-        //return MaterialPageRoute(builder: (ctx)=> const CategoriesScreen());
-
-
+        '/': (ctx) => TabsScreen(),
+        '/category-meals': (ctx) => const CategoryMealsScreen(),
+        '/meal-detail': (ctx) => const MealDetailScreen()
       },
-      onUnknownRoute: (settings){
-        print("onUnknownRoute");
-        print(settings.arguments);
-        return MaterialPageRoute(builder: (ctx)=> const CategoriesScreen());
-
-      },
+      // onGenerateRoute: (settings){
+      //   print("onGenerateRoute");
+      //   print(settings.arguments);
+      //   //return MaterialPageRoute(builder: (ctx)=> const CategoriesScreen());
+      //
+      //
+      // },
+      // onUnknownRoute: (settings){
+      //   print("onUnknownRoute");
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx)=> const CategoriesScreen());
+      //
+      // },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
-
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -76,15 +74,11 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -100,10 +94,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('DeliMeals'),
       ),
       body: const Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-          child: Text('Navigation Time!')
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Text(
+              'Navigation Time!')), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
